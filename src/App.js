b34b91b0fdde59ca9181c75a0ef848544cc570d6
MaduraMadushan <45748038+MaduraMadushan@users.noreply.github.com>
2019-03-26 20:08:43 +0530

    Venue NFO component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Header from "./components/Header_Footer/Header";
 import Features from "./components/Featured";
+import VenueNfo from "./components/venueNfo";
 
 const App = () => {
   return (
-    <div className="App" style={{ height: "1500px", background: "red" }}>
+    <div className="App" style={{ height: "1500px" }}>
       <Header />
       <Features />
+      <VenueNfo />
     </div>
   );
 };
