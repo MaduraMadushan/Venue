2a8bd2df9b8ef02d4a71762eece3d7c99f16378e
MaduraMadushan <45748038+MaduraMadushan@users.noreply.github.com>
2019-03-26 18:26:31 +0530

    Slider section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./resources/styles.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Header from "./components/Header_Footer/Header";
+import Features from "./components/Featured";
 
 const App = () => {
   return (
     <div className="App" style={{ height: "1500px", background: "red" }}>
       <Header />
+      <Features />
     </div>
   );
 };
